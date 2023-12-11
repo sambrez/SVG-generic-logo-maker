f77358bcd2f217108e682487c85a134f1b0e7092
Sam Brez <sambreslin02@gmail.com>
2023-12-10 20:25:42 -0500

    index.js final comments

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # SVG-generic-logo-maker
+
+## Description
+This repository contains Javascript code, executed with Node.js, that generates a quality README.md file for developers when building projects in GitHub. This project also utilizes npm packages inquirer and fs. When initialized, the app prompts the user to answer a series of questions relating to their project. Based on user's responses, a markdwon file is created with the following headers and subheaders: Title, Description, Installation, Usage, Contributing, Tests, Licensing, and Questions. Responses are added as content to the corresponding headers within the markdown file. Created markdown files also contain a table of contents as well as a badge and link to the selected licensing associated with the project.
+
+This is the first project I have completed that uses the node.js environment for executing JavaScript code. I found that I was able to grasp this topic more readily than other topics covered so far in the Coding Bootcamp. I have enjoyed the first introduction to backend coding and working outside the browser and I look forward to diving deeper into backend coding.
+
+## Installation
+N/A
+
+## Usage
+<!-- google drive link to video -->
+
+## Credits
+For this project I greatly utilized the documentation from inquirer. Specifically, I heavily referenced the documentation regarding the validate input property and choices input property when creating the prompt for licensing. The validate property I used to limit the user to selected a maximum of a singular license when answering the prompts.
+
+I would also like to acknowledge GitHub user lukas-h and their license-badges.md file. I used this resource when creating the function for rendering a badge and a link for different licenses within the license section of the generated markdown file. This resource can be found here: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
+## Links
+GitHub: https://github.com/sambrez/SVG-generic-logo-maker
--- index.js
+++ index.js
@@ -1,34 +1,46 @@
+// established variables for imported packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Circle, Square, Triangle} = require('./library/shapes.js');
 
+// function that generates the content of the svg file
 function generateSVG(data) {
     
+    // establishes the text content and color for the svg based on user input
     const characters = `<text x="153" y="125" font-size="45" font-family="script" text-anchor="middle" text-align="center" fill="${data.textColor}">${data.text}</text>`;
 
+    // establishes the shape and shape color for the svg based on user input utilizing the imported classes from the shapes.js file
     function chosenShape () {
         if (data.shape[0] === 'Circle') {
+            // creates new instance of 'Circle" class
             const circle = new Circle;
+            // sets the shape's color by passing in user input
             const circleColor = circle.setColor(data.shapeColor);
             return circle.render(circleColor);
+
         } else if (data.shape[0] === 'Square') {
+            // // creates new instance of 'Square" class
             const square = new Square;
+            // sets the shape's color by passing in user input
             const squareColor = square.setColor(data.shapeColor);
             return square.render(squareColor);
+
         } else {
+            // // creates new instance of 'Triangle" class
             const triangle = new Triangle;
+            // sets the shape's color by passing in user input
             const triangleColor = triangle.setColor(data.shapeColor);
             return triangle.render(triangleColor);
         }
     }
 
-    // const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${characters}</svg>`;
-    // const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${chosenShape()}</svg>`;
+    // creates variable of svg content with combined text and shape features
     const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${chosenShape()}${characters}</svg>`;
     
     return svg;
 }
 
+// questions used to prompt users to set the features of their logo
 const questions = [
     {
         type: 'input',
@@ -85,13 +97,12 @@ const questions = [
                 return true;
             } else {
                 return 'Error: please set a shape color';
-                
             }
         }
     },
 ];
 
-
+// function that writes the svg file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, error => {
         if (error) {
@@ -104,9 +115,10 @@ function writeToFile(fileName, data) {
 
 function init() {
     inquirer
+        // function that prompts user to answer questions
         .prompt(questions)
+        // responses of user then passed to the writeToFile function
         .then(data => {
-            console.log(data);
             const logoData = generateSVG(data);
             writeToFile('logo.svg', logoData);
         })
@@ -117,5 +129,5 @@ function init() {
         });
 };
 
-// Function call
+// app intiation function call
 init();
